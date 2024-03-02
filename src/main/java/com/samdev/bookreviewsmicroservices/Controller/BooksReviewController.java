d9dddac4f97779b821c71d9b5b526c4122392d98
sammy ocharo <obanyisammy22@gmail.com>
2024-03-02 23:19:46 +0300

    initial books review microservice

--- src/main/java/com/samdev/bookreviewsmicroservices/Controller/BooksReviewController.java
+++ src/main/java/com/samdev/bookreviewsmicroservices/Controller/BooksReviewController.java
@@ -6,4 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/apps/api/v1/books-reviews/")
 public class BooksReviewController {
+
+    //create review
+    //see all reviews
+    //see most liked reviews
+    //filter by likes
+    //see total number of reviews
+    //see books with more negative comments
+
 }
