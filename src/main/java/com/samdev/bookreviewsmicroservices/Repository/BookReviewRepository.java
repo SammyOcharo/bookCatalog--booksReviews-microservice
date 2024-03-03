58bcb6682a1a3369804a1907822482d248bbc875
sammy ocharo <obanyisammy22@gmail.com>
2024-03-03 23:22:58 +0300

    minor change

--- src/main/java/com/samdev/bookreviewsmicroservices/Repository/BookReviewRepository.java
+++ src/main/java/com/samdev/bookreviewsmicroservices/Repository/BookReviewRepository.java
@@ -4,6 +4,10 @@ import com.samdev.bookreviewsmicroservices.Entity.BooksReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookReviewRepository extends JpaRepository<BooksReviewEntity, Long> {
+
+    Optional<BooksReviewEntity> findByReviewLanguage(String reviewLanguage);
 }
